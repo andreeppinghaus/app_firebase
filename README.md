71429ac998e0cb183e33b084f68bfac70c94cee4
Andre Eppinghaus <andreeppinghaus@gmail.com>
2021-05-10 23:17:37 -0300

    commit inicial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # app_firebase
+
+Aplicativo de exemplo com autenticação no Firebase
+
+flutter pub get
---
+++ android/app/src/main/kotlin/com/codeworker/app_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.codeworker.app_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
